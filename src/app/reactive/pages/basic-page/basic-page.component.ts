72afed5c3242bb8ea2b09fb421f7dba998e20f31
Roberto Tores <rocatoro18@outlook.com>
2024-10-22 15:43:27 -0700

    .

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const xbox = {
+  name: 'Xbox Series X',
+  price: 499,
+  inStorage: 10
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   private fb: FormBuilder = new FormBuilder();
   // IMPORTAR REACTIVE FORMS MODULE DONDE SE ESTE UTILIZANDO EL FORM GROUP
@@ -17,14 +23,49 @@ export class BasicPageComponent {
 
   constructor(){}
 
+  ngOnInit(): void {
+    //this.myForm.reset(xbox)
+  }
+
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null{
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracters.`;
+
+      }
+    }
+
+    return null;
+  }
+
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0]
+    name: ['',[Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
   });
 
   onSave(): void {
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
     console.log(this.myForm.value);
+
+    this.myForm.reset({price: 0, inStorage:0});
   }
 
 }
