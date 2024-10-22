fa3285f98d9b57015542989f9d6cb30f500d9a87
Roberto Tores <rocatoro18@outlook.com>
2024-10-22 16:49:51 -0700

    Inicio validaciones dinamicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+
+  /*
+  public myForm2 = new FormGroup({
+    favoriteGames: new FormArray([])
+  });
+  */
+
+  private fb: FormBuilder = new FormBuilder();
+
+  constructor(){}
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Halo', Validators.required],
+      ['The Legend of Zelda: Breath of the Wild', Validators.required],
+    ])
+  })
+
+  onSubmit(): void {
+
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
